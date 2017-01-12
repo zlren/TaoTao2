6e3bfab983e0d63e2354175945b08e9d1f28c3a5
zlren <97531314@qq.com>
2017-01-12 21:15:09 +0800

    第一天课程完成

---
+++ taotao-usermanagement/src/main/java/lab/zlren/taotao/controller/RestUserController.java
@@ -0,0 +1,111 @@
+package lab.zlren.taotao.controller;
+
+import lab.zlren.taotao.mybatis.pojo.User;
+import lab.zlren.taotao.service.NewUserService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@RequestMapping("restful/user")
+@Controller
+public class RestUserController {
+
+    Logger logger = Logger.getLogger(RestUserController.class);
+
+    @Autowired
+    private NewUserService newUserService;
+
+    /**
+     * 根据用户id查询用户数据
+     * 
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    @ResponseBody // 如果是ResponseEntity可以不加responsebody
+    public ResponseEntity<User> queryUserById(@PathVariable("id") Long id) {
+        try {
+            User user = this.newUserService.queryUserById(id);
+            if (null == user) {
+                // 资源不存在，响应404
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+            }
+
+            // 200
+            // return ResponseEntity.status(HttpStatus.OK).body(user);
+            return ResponseEntity.ok(user);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 500
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
+    /**
+     * 新增用户
+     * 
+     * @param user
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> saveUser(User user) {
+
+        logger.debug("进来了");
+
+        try {
+            this.newUserService.saveUser(user);
+            logger.info(user);
+            return ResponseEntity.status(HttpStatus.CREATED).build(); // 如果是Void就用build
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        // 500
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
+    /**
+     * 更新用户资源
+     * 
+     * @param user
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Void> updateUser(User user) {
+        try {
+            this.newUserService.updateUser(user);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 500
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+
+    /**
+     * 删除用户资源
+     * 
+     * @param id
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteUser(@RequestParam(value = "id", defaultValue = "0") Long id) {
+        try {
+            if (id.intValue() == 0) {
+                // 请求参数有误
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
+            this.newUserService.deleteUserById(id);
+            // 204
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // 500
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+    }
+}
